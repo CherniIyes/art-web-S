a023ddb6f7be73d082098290a585a3e6e036379b
BadisBoughanmi <badisboughanmi555@gmail.com>
2024-01-24 13:37:55 +0100

    f

--- back-end/Routes/artRoutes.js
+++ back-end/Routes/artRoutes.js
@@ -6,7 +6,7 @@ const routes =  express.Router()
 routes.get("/art/getall",controller.getall)
 routes.get("/art/getone",controller.getone)
 routes.get("/art/add",controller.add)
-routes.get("/art/update",controller.update)
-routes.get("/art/DELETE",controller.DELETE)
+routes.get("/art/update/:id",controller.update)
+routes.get("/art/DELETE/:id",controller.DELETE)
 
 module.exports = routes;
--- back-end/Routes/artistRoutes.js
+++ back-end/Routes/artistRoutes.js
@@ -6,7 +6,7 @@ const routes =  express.Router()
 routes.get("/getall",controller.getall)
 routes.get("/getone",controller.getone)
 routes.get("/add",controller.add)
-routes.get("/update",controller.update)
-routes.get("/DELETE",controller.DELETE)
+routes.get("/update/:id",controller.update)
+routes.get("/DELETE/:id",controller.DELETE)
 
 module.exports = routes;
